b841051e88aaacfb5e362b3723ab7a83cb1acd05
farfadetmalice83-collab <farfadetmalice83@gmail.com>
2026-04-04 15:07:46 +0200

    Update and rename stats.js to netlify/functions/stats.js

--- netlify/functions/stats.js
+++ netlify/functions/stats.js
@@ -1,18 +1,11 @@
 exports.handler = async function() {
-  const API_KEY = '89a64c6c883849af9b36216912f484f8';
-
   const res = await fetch('https://api.football-data.org/v4/competitions/PPL/standings', {
-    headers: { 'X-Auth-Token': API_KEY }
+    headers: { 'X-Auth-Token': '89a64c6c883849af9b36216912f484f8' }
   });
-
   const data = await res.json();
-
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
     body: JSON.stringify(data)
   };
 };
